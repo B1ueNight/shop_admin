faefe53da02d99d1a8aaede718f63f2276817a77
CsixHsix <tree3841@gmail.com>
2022-01-12 18:03:43 +0900

    프로젝트 파일 업로드

---
+++ src/main/java/com/bluenight/shoopingmall_admin/ShoopingmallAdminApplication.java
@@ -0,0 +1,13 @@
+package com.bluenight.shoopingmall_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoopingmallAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoopingmallAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluenight/shoopingmall_admin/ShoopingmallAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluenight.shoopingmall_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoopingmallAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
