af06bd17f3aeb4ae3ea2c2e1118b5de665834b1c
CsixHsix <tree3841@gmail.com>
2022-01-24 15:37:37 +0900

    회원 관리

---
+++ src/main/java/com/bluenight/shoppingmall_admin/mapper/MemberMapper.java
@@ -0,0 +1,19 @@
+package com.bluenight.shoppingmall_admin.mapper;
+
+import java.util.List;
+
+import com.bluenight.shoppingmall_admin.data.MemberVO;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberMapper {
+    List<MemberVO> getMemberList(String keyword, String type, Integer offset);
+    Integer getMemberCnt(String keyword, String type);
+    MemberVO getMemberBySeq(Integer seq);
+    void insertmember(MemberVO data);
+    void updatemember(MemberVO data);
+    void deletemember(Integer seq);
+    
+    Integer isExistEmail(String email);
+}
